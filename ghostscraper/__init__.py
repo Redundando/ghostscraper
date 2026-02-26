d36e243485d17ead45cb5a025226578f7f5f312e
Arved (Work Laptop) <klojarve@audible.de>
2026-02-26 10:19:15 +0100

    Bump version to 0.6.0

--- ghostscraper/__init__.py
+++ ghostscraper/__init__.py
@@ -20,7 +20,7 @@ from .ghost_scraper import GhostScraper
 from .playwright_installer import check_browser_installed, install_browser
 from .config import ScraperDefaults
 
-__version__ = "0.5.0"
+__version__ = "0.6.0"
 __all__ = [
     "GhostScraper",
     "PlaywrightScraper",
