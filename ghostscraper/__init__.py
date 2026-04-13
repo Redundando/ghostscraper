9ef05460f98d57e10f53c5adc02c9a8efe986ce8
Arved (Work Laptop) <klojarve@audible.de>
2026-04-13 18:01:24 +0200

    Bump version to 0.9.11

--- ghostscraper/__init__.py
+++ ghostscraper/__init__.py
@@ -21,7 +21,7 @@ from .playwright_installer import check_browser_installed, install_browser
 from .config import ScraperDefaults
 from .stream import ScrapeStream, StreamStatus
 
-__version__ = "0.9.10"
+__version__ = "0.9.11"
 __all__ = [
     "GhostScraper",
     "PlaywrightScraper",
